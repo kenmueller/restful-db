c991004c8ebfaded231984a3a00ac804fe711c03
Ken Mueller <kenmueller0@gmail.com>
2019-10-07 16:56:14 -0700

    finished List all records and removed errors

--- functions/src/api.ts
+++ functions/src/api.ts
@@ -9,36 +9,34 @@ export const api = functions.https.onRequest(app)
 
 // List all records
 app.get('/:path', (req, res) =>
-	firestore.collection(`api/${req.params.path}`).get().then(snapshot =>
-		snapshot.docs.map(doc =>
-			Object.entries(doc.data()).map(([id, json]: [string, string]) => ({
-				...JSON.parse(json), id
-			}))
-		)
+	firestore.doc(`api/${req.params.path}`).get().then(snapshot =>
+		Object.entries(snapshot.data() || []).map(([id, json]: [string, string]) => ({
+			...JSON.parse(json), id
+		}))
 	).then(res.json.bind(res)).catch(() => res.status(500).json([]))
 )
 
 // Get record
-app.get('/:path/:id', (req, res) => {
+// app.get('/:path/:id', (req, res) => {
 	
-})
+// })
 
 // Create record
-app.post('/:path', (req, res) => {
+// app.post('/:path', (req, res) => {
 	
-})
+// })
 
 // Update all properties of a record
-app.put('/:path/:id', (req, res) => {
+// app.put('/:path/:id', (req, res) => {
 	
-})
+// })
 
 // Update some properties of a record
-app.patch('/:path/:id', (req, res) => {
+// app.patch('/:path/:id', (req, res) => {
 	
-})
+// })
 
 // Delete record
-app.delete('/:path/:id', (req, res) => {
+// app.delete('/:path/:id', (req, res) => {
 	
-})
+// })
