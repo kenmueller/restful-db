382da6b638b08c15cf5229040a03d9fbe565ba5f
Ken Mueller <kenmueller0@gmail.com>
2019-10-07 18:10:37 -0700

    added projects

--- functions/src/api.ts
+++ functions/src/api.ts
@@ -10,11 +10,11 @@ const firestore = admin.firestore()
 
 export const api = functions.https.onRequest(app)
 
-const documentFromPath = (path: string): FirebaseFirestore.DocumentReference =>
-	firestore.doc(`api/${path}`)
+const documentFromPath = (project: string, path: string): FirebaseFirestore.DocumentReference =>
+	firestore.doc(`${project}/${path}`)
 
-const snapshotFromPath = (path: string): Promise<FirebaseFirestore.DocumentSnapshot> =>
-	documentFromPath(path).get()
+const snapshotFromPath = (project: string, path: string): Promise<FirebaseFirestore.DocumentSnapshot> =>
+	documentFromPath(project, path).get()
 
 const createSendableRecord = (id: string, json: string): { id: string } & object => ({
 	...JSON.parse(json),
@@ -25,35 +25,36 @@ const newId = (): string =>
 	secure.newId(ID_LENGTH)
 
 // List all records
-app.get('/:path', (req, res) =>
-	snapshotFromPath(req.params.path).then(snapshot =>
-		Object.entries(snapshot.data() || []).map(([id, json]: [string, string]) =>
+app.get('/:project/:path', (req, res) => {
+	const { project, path } = req.params
+	return snapshotFromPath(project, path).then(snapshot =>
+		Object.entries(snapshot.data() || {}).map(([id, json]: [string, string]) =>
 			createSendableRecord(id, json)
 		)
 	).then(res.json.bind(res)).catch(() => res.status(500).json([]))
-)
+})
 
 // Get record
-app.get('/:path/:id', (req, res) => {
-	const { path, id } = req.params
-	return snapshotFromPath(path).then(snapshot => {
+app.get('/:project/:path/:id', (req, res) => {
+	const { project, path, id } = req.params
+	return snapshotFromPath(project, path).then(snapshot => {
 		const json = snapshot.get(id)
-		json
+		return json
 			? res.json(createSendableRecord(id, json))
 			: res.status(404).json({})
 	})
 })
 
 // Create record
-app.post('/:path', (req, res) => {
+app.post('/:project/:path', (req, res) => {
 	const { params, body } = req
-	const documentReference = documentFromPath(params.path)
+	const { project, path } = params
 	const id = newId()
 	const updateObject = { [id]: JSON.stringify({ ...body, id: undefined }) }
-	return documentReference.get().then(document =>
-		document.exists
-			? documentReference.update(updateObject)
-			: documentReference.set(updateObject)
+	return snapshotFromPath(project, path).then(snapshot =>
+		snapshot.exists
+			? snapshot.ref.update(updateObject)
+			: snapshot.ref.set(updateObject)
 	).then(() => res.json({ ...body, id })).catch(() => res.status(500).json({}))
 })
 
@@ -61,7 +62,10 @@ app.post('/:path', (req, res) => {
 // app.put('/:path/:id', (req, res) => {
 // 	const { params, body } = req
 // 	const { path, id } = params
-	
+// 	const documentReference = documentFromPath(path)
+// 	return documentReference.get().then(snapshot =>
+		
+// 	)
 // })
 
 // Update some properties of a record
