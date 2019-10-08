ac26f0e44a5b349b0a6fd114c2621064a535336a
Ken Mueller <kenmueller0@gmail.com>
2019-10-08 01:12:17 -0700

    renders App component

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<h1>Restful DB by Ken Mueller</h1>, document.getElementById('root'))
+import App from './components/App'
+
+ReactDOM.render(<App />, document.getElementById('root'))
 
 serviceWorker.register()
