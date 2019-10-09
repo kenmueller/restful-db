f9581e49b0436a8320222b35a980826aca15c62a
Ken Mueller <kenmueller0@gmail.com>
2019-10-09 14:36:33 -0700

    base for Documentation

--- src/components/Documentation.tsx
+++ src/components/Documentation.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import * as Bulma from 'react-bulma-components'
 
 import { DocumentationProps } from '../types/props'
 import { AppState } from '../reducers'
+import Navbar from './Navbar'
+
+import '../scss/Documentation.scss'
 
 class Documentation extends React.Component<DocumentationProps> {
 	render() {
-		return <>Documentation</>
+		return (
+			<Bulma.Hero size="medium" textWeight="bold" className="documentation-hero">
+				<Navbar>
+					Documentation
+				</Navbar>
+			</Bulma.Hero>
+		)
 	}
 }
 
